888947e32363661ea1186c1c357d31b2806f1c08
zhouyu <1009417330@qq.com>
2025-06-05 11:28:37 +0800

    fix:修复扩展思考开关的事件处理逻辑

--- src/renderer/components.d.ts
+++ src/renderer/components.d.ts
@@ -31,6 +31,7 @@ declare module 'vue' {
     ARow: typeof import('ant-design-vue/es')['Row']
     ASelect: typeof import('ant-design-vue/es')['Select']
     ASelectOption: typeof import('ant-design-vue/es')['SelectOption']
+    ASlider: typeof import('ant-design-vue/es')['Slider']
     ASpace: typeof import('ant-design-vue/es')['Space']
     ASwitch: typeof import('ant-design-vue/es')['Switch']
     ATable: typeof import('ant-design-vue/es')['Table']
