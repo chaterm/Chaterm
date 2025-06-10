0907e6fcad7ebb1fc8ea42b34db110808c26d227
zhouyu <1009417330@qq.com>
2025-06-10 14:03:14 +0800

    fix: Correct the connection condition judgment

--- src/main/agent/core/task/index.ts
+++ src/main/agent/core/task/index.ts
@@ -217,7 +217,7 @@ export class Task {
       return
     }
     let terminalInfo: RemoteTerminalInfo | null = null
-    if (this.hosts[0].connection === 'personal') {
+    if (this.hosts[0].organizationId === 'personal') {
       let terminalUuid = this.hosts[0].uuid
       const connectionInfo = await connectAssetInfo(terminalUuid)
       connectionInfo.type = 'ssh'
@@ -225,7 +225,6 @@ export class Task {
       terminalInfo = await this.remoteTerminalManager.createTerminal()
     } else {
       // websocket
-
       let authData = {
         user: 'test',
         email: 'test@gmail.com',
