a1a516ac8890eba0fa4720e7e2fb1d89e2ac78f0
zhouyu <1009417330@qq.com>
2025-06-11 14:44:32 +0800

    feat: And update the command message processing logic to support specific chat types

--- src/renderer/components.d.ts
+++ src/renderer/components.d.ts
@@ -25,6 +25,7 @@ declare module 'vue' {
     AInputSearch: typeof import('ant-design-vue/es')['InputSearch']
     AMenu: typeof import('ant-design-vue/es')['Menu']
     AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
+    AModal: typeof import('ant-design-vue/es')['Modal']
     ARadio: typeof import('ant-design-vue/es')['Radio']
     ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
