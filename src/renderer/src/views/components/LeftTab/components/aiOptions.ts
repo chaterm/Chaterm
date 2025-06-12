35d8d04c9a4d52e313bc1cc1090e674701fcba3e
yin_ding <531895835@qq.com>
2025-06-12 11:47:47 +0800

    [AiTab] fix model choose

--- src/renderer/src/views/components/LeftTab/components/aiOptions.ts
+++ src/renderer/src/views/components/LeftTab/components/aiOptions.ts
@@ -35,21 +35,21 @@ export const aiModelOptions = [
 
 export const litellmAiModelOptions = [
   { value: 'claude-sonnet-4', label: 'claude-sonnet-4' },
-  { value: 'claude-opus-4', label: 'claude-opus-4' },
-  { value: 'claude-3-7-sonnet', label: 'claude-3-7-sonnet' },
-  { value: 'claude-3-5-haiku', label: 'claude-3-5-haiku' },
-  { value: 'claude-3-5-sonnet', label: 'claude-3-5-sonnet' },
-  { value: 'claude-3-opus', label: 'claude-3-opus' },
-  { value: 'gpt-4.1', label: 'gpt-4.1' },
-  { value: 'gpt-4.1-mini', label: 'gpt-4.1-mini' },
-  { value: 'gpt-4.1-nano', label: 'gpt-4.1-nano' },
-  { value: 'gpt-4o', label: 'gpt-4o' },
-  { value: 'qwen-plus-latest', label: 'qwen-plus-latest' },
-  { value: 'qwen3-235b-a22b', label: 'qwen3-235b-a22b' },
-  { value: 'qwen3-32b', label: 'qwen3-32b' },
-  { value: 'qwen3-30b-a3b', label: 'qwen3-30b-a3b' },
-  { value: 'qwen3-14b', label: 'qwen3-14b' },
-  { value: 'qwen3-8b', label: 'qwen3-8b' },
-  { value: 'deepseek-r1', label: 'deepseek-r1' },
-  { value: 'deepseek-v3-0324', label: 'deepseek-v3-0324' }
-] 
+  // { value: 'claude-opus-4', label: 'claude-opus-4' },
+  { value: 'claude-3-7-sonnet', label: 'claude-3-7-sonnet' }
+  // { value: 'claude-3-5-haiku', label: 'claude-3-5-haiku' },
+  // { value: 'claude-3-5-sonnet', label: 'claude-3-5-sonnet' },
+  // { value: 'claude-3-opus', label: 'claude-3-opus' },
+  // { value: 'gpt-4.1', label: 'gpt-4.1' },
+  // { value: 'gpt-4.1-mini', label: 'gpt-4.1-mini' },
+  // { value: 'gpt-4.1-nano', label: 'gpt-4.1-nano' },
+  // { value: 'gpt-4o', label: 'gpt-4o' },
+  // { value: 'qwen-plus-latest', label: 'qwen-plus-latest' },
+  // { value: 'qwen3-235b-a22b', label: 'qwen3-235b-a22b' },
+  // { value: 'qwen3-32b', label: 'qwen3-32b' },
+  // { value: 'qwen3-30b-a3b', label: 'qwen3-30b-a3b' },
+  // { value: 'qwen3-14b', label: 'qwen3-14b' },
+  // { value: 'qwen3-8b', label: 'qwen3-8b' },
+  // { value: 'deepseek-r1', label: 'deepseek-r1' },
+  // { value: 'deepseek-v3-0324', label: 'deepseek-v3-0324' }
+]
