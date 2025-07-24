2cda72a35f24d7f5094a130101cb651f8bd047cf
yin_ding <531895835@qq.com>
2025-07-24 19:02:54 +0800

    [login] support windows dev

--- src/renderer/src/views/auth/login.vue
+++ src/renderer/src/views/auth/login.vue
@@ -273,6 +273,7 @@ onMounted(async () => {
   const api = window.api as any
   platform.value = await api.getPlatform()
   isDevWin.value = import.meta.env.MODE === 'development'
+  // isDevWin.value = import.meta.env.MODE === 'development' && platform.value === 'win32'
   await captureButtonClick(LoginFunnelEvents.ENTER_LOGIN_PAGE)
   if (!isDevWin.value) {
     // 监听外部登录成功事件
