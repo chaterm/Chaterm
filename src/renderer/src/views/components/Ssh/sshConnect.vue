ebd72c2cca2ca90fe6935cbb957e64cbdb9cc9c4
allen_zhou <allen_zhou@intsig.net>
2026-03-23 11:26:57 +0800

    fix: update cursorAccent colors in K8sConnect and sshConnect components for improved visibility

--- src/renderer/src/views/components/Ssh/sshConnect.vue
+++ src/renderer/src/views/components/Ssh/sshConnect.vue
@@ -503,7 +503,7 @@ onMounted(async () => {
               background: config.background?.image ? 'rgba(245, 245, 245, 0.82)' : '#f5f5f5',
               foreground: '#000000',
               cursor: '#000000',
-              cursorAccent: '#000000',
+              cursorAccent: '#f5f5f5',
               selectionBackground: '#add6ff80',
               selectionInactiveBackground: '#add6ff5a'
             }
@@ -511,7 +511,7 @@ onMounted(async () => {
               background: config.background?.image ? 'transparent' : '#141414',
               foreground: '#e0e0e0',
               cursor: '#e0e0e0',
-              cursorAccent: '#e0e0e0',
+              cursorAccent: '#141414',
               selectionBackground: 'rgba(255, 255, 255, 0.3)',
               selectionInactiveBackground: 'rgba(255, 255, 255, 0.2)'
             }
@@ -777,7 +777,7 @@ onMounted(async () => {
               background: config.background?.image ? 'rgba(245, 245, 245, 0.82)' : '#f5f5f5',
               foreground: '#000000',
               cursor: '#000000',
-              cursorAccent: '#000000',
+              cursorAccent: '#f5f5f5',
               selectionBackground: '#add6ff80',
               selectionInactiveBackground: '#add6ff5a'
             }
@@ -785,7 +785,7 @@ onMounted(async () => {
               background: configStore.getUserConfig.background.image ? 'transparent' : '#141414',
               foreground: '#e0e0e0',
               cursor: '#e0e0e0',
-              cursorAccent: '#e0e0e0',
+              cursorAccent: '#141414',
               selectionBackground: 'rgba(255, 255, 255, 0.3)',
               selectionInactiveBackground: 'rgba(255, 255, 255, 0.2)'
             }
